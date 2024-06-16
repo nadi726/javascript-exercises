dac464068c7d929c3a225315e0e9e6e553d3b02e
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-06-16 17:33:02 +0300

    Finish exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(oldString, numOfTimes) {
+    if (numOfTimes < 0) return "ERROR";
 
+    let newString = '';
+    for (let i = numOfTimes; i > 0; i--) {
+        newString += oldString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
