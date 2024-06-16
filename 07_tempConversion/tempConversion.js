4367ab336a3b4244562f113d7c2c1ddd3e88fc90
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-06-16 21:25:07 +0300

    Finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let degrees = (f - 32) * (5 / 9)
+  return roundTo(degrees, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let degrees = c * (9 / 5) + 32;
+  return roundTo(degrees, 1)
 };
+
+function roundTo(num, decimals) {
+  let powers = 10 ** decimals
+  return Math.round(num * powers) / powers;
+}
 
 // Do not edit below this line
 module.exports = {
