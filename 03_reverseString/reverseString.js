84a899b2785f503dd62de429f94757847dee9364
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-06-16 17:38:17 +0300

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let charsArray = str.split("");
+    charsArray.reverse();
+    let reversedStr = charsArray.join("");
 
+    return reversedStr;
 };
 
 // Do not edit below this line
