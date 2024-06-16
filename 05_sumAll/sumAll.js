35c68b9d582a413e8d3dc70df0ce9c41eadbdf01
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-06-16 18:47:31 +0300

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(m, n) {
+    if (typeof m != "number" || typeof n != "number" 
+        || m < 0 || n < 0) {
+        return "ERROR";
+    }
+    
+    // Ensure that the larger number is last
+    if (m > n) return sumAll(n, m);
 
+    let sum = 0;
+    for (let currentNum = m; currentNum <= n; currentNum++) {
+        sum += currentNum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
