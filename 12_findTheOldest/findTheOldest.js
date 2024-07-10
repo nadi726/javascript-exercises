2697acb3e749ffbb9bc34f1c73513279c6e35e50
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-07-10 20:42:00 +0300

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    // This object is used inside the reduce
+    // in order to avoid calculating the accumulator's
+    // age at each check.
+    let computedObject = {object: arr[0], age: calculateAge(arr[0])};
+    
+    return arr.reduce(
+        (oldest, current) => {
+            let ageOfCurrent = calculateAge(current);
+            if (ageOfCurrent > oldest.age) {
+                return {object: current, age: ageOfCurrent};
+            }
+            return oldest;
+        },
+        computedObject
+    ).object
 };
+
+const calculateAge = function(person) {
+    let endYear;
+    if ("yearOfDeath" in person) {
+        endYear = person.yearOfDeath;
+    } else {
+        endYear = new Date().getFullYear();
+    }
+    return endYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
