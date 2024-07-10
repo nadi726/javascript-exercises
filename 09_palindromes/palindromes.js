e81ff58b1026ea5a3e147ccba5a7516d4365a1db
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-07-10 16:42:24 +0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"").toLowerCase();
+    const reveredStr = cleanedStr.split("").reverse().join("");
+    return reveredStr === cleanedStr;
 };
 
 // Do not edit below this line
