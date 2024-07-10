f287173a93a3b1bbfdfad34cdd72c90845f002e9
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-07-10 17:54:14 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    
+    let a = 0;
+    let b = 1;
+    let c = a + b;
+    for (let i = 0; i < n - 1; i++) { 
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
